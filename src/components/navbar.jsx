e4821a13780fbf8396dcebf39bc039ad6f3abe70
ukabyl <umkabylbekov@gmail.com>
2020-04-29 17:28:55 +0500

    basic structure and navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import { Link } from 'react-router-dom';
+import { FaAlignRight } from 'react-icons/fa';
+
+export default  class Navbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggleHandler = () => {
+        this.setState(({ isOpen }) => ({ isOpen: !isOpen }))
+    }
+
+    render() {
+        return (
+            <nav className="navbar">
+                <div className="nav-center">
+                    <div className="nav-header">
+                        <Link to="/">
+                            <img src={logo} alt="Beach Resort"/>
+                        </Link>
+                        <button 
+                            className="nav-btn" type="button"
+                            onClick={this.toggleHandler}
+                            >
+                            <FaAlignRight className="nav-icon" />
+                        </button>
+                    </div>
+                    <ul 
+                        className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}>
+                    
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/rooms/">Rooms</Link>
+                        </li>
+                    </ul>  
+                </div>
+            </nav>
+        );
+    }
+}
